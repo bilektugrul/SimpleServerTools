923242670639b595c3eeaf7af4a4f56614f29a71
Ali Arslan <aliarslann5500@gmail.com>
2021-04-25 18:31:56 +0300

    Make msg command more readable

--- src/main/java/io/github/bilektugrul/simpleservertools/commands/msg/MessageCommand.java
+++ src/main/java/io/github/bilektugrul/simpleservertools/commands/msg/MessageCommand.java
@@ -30,69 +30,73 @@ public class MessageCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
-        if (sender.hasPermission("sst.msg")) {
-            if (args.length >= 1) {
-                String format = Utils.getString("other-messages.msg.format", sender)
-                        .replace("%from%", Utils.matchName(sender));
-
-                String to;
-                boolean reply = false;
-
-                if (label.startsWith("r") || label.equalsIgnoreCase("yanıt")) {
-                    reply = true;
-                    to = replyList.get(sender.getName());
-                } else {
-                    to = args[0];
-                }
-
-                if (to != null) {
-                    Player toPlayer = Bukkit.getPlayer(to);
-                    if (toPlayer != null) {
-                        String toPlayerName = toPlayer.getName();
-                        User senderUser = null;
-
-                        if (sender instanceof Player) {
-                            senderUser = userManager.getUser((Player) sender);
-                        }
-
-                        User toUser = userManager.getUser(toPlayer);
-                        UUID toUserUUID = toUser.getUUID();
-                        boolean vanishMsgState = Utils.getBoolean("vanish.messages-to-vanished-players", false);
-
-                        if (!vanishMsgState && vanishManager.isVanished(toUserUUID)) {
-                            sender.sendMessage(Utils.getString("other-messages.msg.not-found", sender));
-                            return true;
-                        }
-
-                        if (toUser.isAcceptingMsg()) {
-                            if (senderUser != null && senderUser.isAcceptingMsg()) {
-                                if (!reply && args.length == 1) {
-                                    sender.sendMessage(Utils.getString("other-messages.msg.wrong-usage", sender));
-                                } else {
-                                    format = format.replace("%to%", toPlayerName)
-                                            .replace("%msg%", Utils.arrayToString(Arrays.copyOfRange(args, reply ? 0 : 1, args.length), sender, false, false));
-                                    toPlayer.sendMessage(format);
-                                    sender.sendMessage(format);
-                                    replyList.put(sender.getName(), toPlayerName);
-                                }
-                            } else {
-                                sender.sendMessage(Utils.getString("other-messages.msg.closed-self", sender));
-                            }
-                        } else {
-                            sender.sendMessage(Utils.getString("other-messages.msg.closed", sender)
-                                .replace("%to%", toPlayerName));
-                        }
-                    } else {
-                        sender.sendMessage(Utils.getString("other-messages.msg.not-found", sender));
-                    }
-                }
-
-            } else {
-                sender.sendMessage(Utils.getString("other-messages.msg.wrong-usage", sender));
-            }
-        } else {
+        if (!sender.hasPermission("sst.msg")) {
             sender.sendMessage(Utils.getString("no-permission", sender));
+            return true;
+        }
+
+        if (args.length == 0) {
+            sender.sendMessage(Utils.getString("other-messages.msg.wrong-usage", sender));
+            return true;
+        }
+
+        String format = Utils.getString("other-messages.msg.format", sender)
+                .replace("%from%", Utils.matchName(sender));
+
+        String to = args[0];
+        boolean reply = false;
+
+        if (label.startsWith("r") || label.equalsIgnoreCase("yanıt")) {
+            reply = true;
+            to = replyList.get(sender.getName());
+        }
+
+        Player toPlayer = Bukkit.getPlayer(to);
+        if (toPlayer == null) {
+            sender.sendMessage(Utils.getString("other-messages.msg.not-found", sender));
+            return true;
+        }
+
+        String toPlayerName = toPlayer.getName();
+        boolean senderAcceptMessages;
+
+        if (sender instanceof Player) {
+            User senderUser = userManager.getUser((Player) sender);
+            senderAcceptMessages = senderUser.isAcceptingMsg();
+        } else {
+            senderAcceptMessages = true;
+        }
+
+        User toUser = userManager.getUser(toPlayer);
+        UUID toUserUUID = toUser.getUUID();
+        boolean vanishMsgState = Utils.getBoolean("vanish.messages-to-vanished-players", false);
+
+        if (!vanishMsgState && vanishManager.isVanished(toUserUUID)) {
+            sender.sendMessage(Utils.getString("other-messages.msg.not-found", sender));
+            return true;
+        }
+
+        if (!toUser.isAcceptingMsg()) {
+            sender.sendMessage(Utils.getString("other-messages.msg.closed", sender)
+                    .replace("%to%", toPlayerName));
+            return true;
+        }
+
+        if (!senderAcceptMessages) {
+            sender.sendMessage(Utils.getString("other-messages.msg.closed-self", sender));
+            return true;
+        }
+
+        if (!reply && args.length == 1) {
+            sender.sendMessage(Utils.getString("other-messages.msg.wrong-usage", sender));
+        } else {
+            format = format.replace("%to%", toPlayerName)
+                    .replace("%msg%", String.join(" ", Arrays.copyOfRange(args, reply ? 0 : 1, args.length)));
+            toPlayer.sendMessage(format);
+            sender.sendMessage(format);
+            replyList.put(sender.getName(), toPlayerName);
         }
         return true;
     }
+
 }
