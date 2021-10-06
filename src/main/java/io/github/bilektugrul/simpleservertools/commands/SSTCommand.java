a867450be10d79010595f4dedb8ba7f465e1ba7a
Ali Arslan <aliarslann5500@gmail.com>
2021-10-06 11:48:51 +0300

    No more empty lines in the end of files

--- src/main/java/io/github/bilektugrul/simpleservertools/commands/SSTCommand.java
+++ src/main/java/io/github/bilektugrul/simpleservertools/commands/SSTCommand.java
@@ -44,4 +44,3 @@ public class SSTCommand implements CommandExecutor {
     }
 
 }
-
--- src/main/java/io/github/bilektugrul/simpleservertools/stuff/MessageType.java
+++ src/main/java/io/github/bilektugrul/simpleservertools/stuff/MessageType.java
@@ -3,4 +3,5 @@ package io.github.bilektugrul.simpleservertools.stuff;
 public enum MessageType {
 
     CHAT, TITLE, ACTIONBAR, CHAT_AND_TITLE, CHAT_AND_BAR, BAR_AND_TITLE, ALL
+
 }
