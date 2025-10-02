f5e044a50c6e2444143f7bbad25e9ecd4fcbfc17
Ali Arslan <aliarslann5500@gmail.com>
2025-10-02 13:21:20 +0300

    Use faster user cache

--- src/main/java/io/github/bilektugrul/simpleservertools/users/UserManager.java
+++ src/main/java/io/github/bilektugrul/simpleservertools/users/UserManager.java
@@ -6,14 +6,12 @@ import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 public class UserManager {
 
     private final SST plugin;
-    private final Set<User> userList = new HashSet<>();
+    private final Map<UUID, User> cachedUsers = new HashMap<>();
 
     public UserManager(SST plugin) {
         this.plugin = plugin;
@@ -26,7 +24,7 @@ public class UserManager {
     public User loadUser(UUID uuid, String name, boolean keep) {
         YamlConfiguration dataFile = YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder() + "/players/" + uuid + ".yml"));
         User user = new User(dataFile, uuid, name);
-        if (keep) userList.add(user);
+        if (keep) cachedUsers.put(uuid, user);
         return user;
     }
 
@@ -36,16 +34,11 @@ public class UserManager {
     }
 
     public User getUser(UUID uuid) {
-        for (User user : userList) {
-            if (user.getUUID().equals(uuid)) {
-                return user;
-            }
-        }
-        return null;
+        return cachedUsers.get(uuid);
     }
 
     public void removeUser(User user) {
-        userList.remove(user);
+        cachedUsers.remove(user.getUUID());
     }
 
     public boolean isTeleporting(User user) {
@@ -61,12 +54,12 @@ public class UserManager {
         return false;
     }
 
-    public Set<User> getUserList() {
-        return new HashSet<>(userList);
+    public Set<User> getAllUsers() {
+        return new HashSet<>(cachedUsers.values());
     }
 
     public void saveUsers() throws IOException {
-        for (User user : userList) {
+        for (User user : cachedUsers.values()) {
             user.save();
         }
     }
